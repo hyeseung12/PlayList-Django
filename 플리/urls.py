283413254ad20eb30fd8e267a9636c1bc257af8b
MsEmily1020 <peace10200khs@gmail.com>
2024-06-06 17:42:41 +0900

    feat: urls, views, templates index.html 연결

---
+++ 플리/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from 플리 import views
+
+app_name = '플리'
+
+urlpatterns = [
+    path('', views.show_index, name='show_index')
+]
--- 플리/views.py
+++ 플리/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def show_index(request):
+    context = {}
+    return render(request, '플리/index.html', context=context)
