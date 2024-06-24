c9193d5b7a4fcf496795b15d053289dde5788da0
MsEmily1020 <peace10200khs@gmail.com>
2024-06-24 10:37:48 +0900

    feat: login html 경로 등록

--- 플리/urls.py
+++ 플리/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from 플리 import views
 
-app_name = 'img'
+app_name = '플리'
 
 urlpatterns = [
-    path('', views.show_index, name='show_index')
+    path('', views.show_index, name='show_index'),
+    path('login/', views.show_login, name='show_login')
 ]
--- 플리/views.py
+++ 플리/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def show_index(request):
     context = {}
     return render(request, '플리/index.html', context=context)
+
+def show_login(request):
+    context = {}
+    return render(request, '플리/login.html', context=context)
