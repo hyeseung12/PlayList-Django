38e1034345affec9becc3ac344d1f77e87a393e5
MsEmily1020 <peace10200khs@gmail.com>
2024-06-25 18:21:49 +0900

    fix: user model 변경

--- 플리/models.py
+++ 플리/models.py
@@ -1,11 +1,47 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, UserManager
 from django.db import models
 
-# Create your models here.
-class User(models.Model):
-    user_id = models.CharField(max_length=20)
-    pw = models.CharField(max_length=20)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+class UserManager(BaseUserManager):
+    def create_user(self, email, nickname, password=None):
+        if not email:
+            raise ValueError('must have user email')
+
+        user = self.model(
+            email = self.normalize_email(email),
+            nickname = nickname
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, nickname, password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            nickname=nickname,
+            password=password
+        )
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    objects = UserManager()
+
+    email = models.EmailField(
+        max_length=255,
+        unique=True,
+    )
+
+    nickname = models.CharField(max_length=20)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nickname']
 
     def __str__(self):
-        return f'{self.id} {self.pw} {self.created_at} {self.updated_at}'
+        return self.nickname
